2c908603f438ae2a86baf1a32965fa71f774d595
seobei <sys1774@nate.com>
2021-06-16 15:30:12 +0900

    spring

---
+++ src/test/java/sample/junit/MyTest1.java
@@ -0,0 +1,40 @@
+package sample.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest1 {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		int i = 3;
+		if (i != 3) {
+			fail("i는 3이 아닙니다.");
+		}
+	}
+	
+	@Test
+	public void equals() {
+		int i = 3;
+		boolean b = true;
+		Object c = null;
+		
+		assertNotEquals(i, 3);
+		assertNotEquals(b, true);
+		assertTrue(b);
+		assertNull(c);
+	}
+	
+	@Test
+	public void notequals() {
+		int i = 5;
+		boolean b = false;
+		Object c = new Object();
+		
+		assertNotEquals(i, 3);
+		assertFalse(b);
+		assertNotNull(c);
+}
+}
